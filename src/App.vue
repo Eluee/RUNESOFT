699de527e87f7918fc10f75ff1328e5f56d538c0
Lbj0820 <101816781+Lbj0820@users.noreply.github.com>
2022-04-01 20:01:34 +0900

    Revert "banner structre"

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,32 @@
 <template>
-  <div class="topbar">
-    <a>Rune Soft</a>
-  </div>
-  <div class="navber">
-    <nav>
-      <router-link to="/">HOME</router-link>
-      <router-link to="/info">INFO</router-link>
-      <router-link to="/project">PROJECT</router-link>
-      <router-link to="/community">COMMUNITY</router-link>
-    </nav>
-  </div>
-  <div class="bannerimg">
-    <img src="@\assets\banner\TopBanner_1.png" />
-  </div>
+  <nav>
+    <router-link to="/">HOME</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/info">Info</router-link> |
+    <router-link to="/community">Community</router-link>
+  </nav>
   <router-view />
 </template>
 
 <style>
-.navber {
-  position: relative;
-  z-index: 2;
-}
-
-.bannerimg {
-  position: relative;
-  z-index: 1;
-  top: -79px;
-}
-
-.bannerimg img {
-  height: 500px;
-}
-
 #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
   text-align: center;
+  color: #2c3e50;
 }
 
 nav {
-  padding-top: 33px;
+  padding: 30px;
 }
 
 nav a {
-  font-family: "Noto Sans KR";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 18px;
-  line-height: 26px;
-  color: #999999;
-  padding: 10px;
-  text-decoration: none;
+  font-weight: bold;
+  color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
-  color: rgb(255, 255, 255);
-}
-
-.topbar {
-  width: 100%;
-  height: 30px;
-  left: 0px;
-  top: 0px;
-  text-align: left;
-}
-
-.topbar a {
-  padding-left: 60px;
-  padding-top: 2px;
-
-  font-family: "Paytone One";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 18px;
-  line-height: 25px;
-  color: #333333;
+  color: #42b983;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,12 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/project",
-    name: "project",
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import("../views/ProjectView.vue"),
+    component: () => import("../views/AboutView.vue"),
   },
   {
     path: "/info",
