005bed8b86425c4545904b1c808c4d574ecbb920
Eluee <touch1027@naver.com>
2022-04-01 17:40:18 +0900

    banner structre

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,12 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
+    path: "/project",
+    name: "project",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import("../views/AboutView.vue"),
+    component: () => import("../views/ProjectView.vue"),
   },
   {
     path: "/info",
