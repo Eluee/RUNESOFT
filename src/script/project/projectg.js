3fec1c758ae164bd9d6b9ee76f5683d18495cda4
lbj <byugjun1253@gmail.com>
2022-07-31 13:41:25 +0900

    last revision

---
+++ src/script/project/projectg.js
@@ -0,0 +1,114 @@
+export default [
+  {
+    id: 0,
+    title: "a프로젝트",
+    image:
+      "https://th.bing.com/th/id/OIP.74ksF_CX0g9PELbBe3lPdQHaHa?w=203&h=203&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+    content: "18년 신축공사한 남향 원룸 ☀️, 공기청정기 제공",
+    image2: "https://gtechbooster.com/media/vue.jpg",
+  },
+  {
+    id: 1,
+    title: "b프로젝트",
+    image:
+      "https://blog.kakaocdn.net/dn/b8cCRr/btqBj57k7zX/70VkmwRvIB6b6Q9krOnIO0/img.jpg",
+    content: "그냥 아무거나 적은 것 제공",
+    image2: "https://gtechbooster.com/media/vue.jpg",
+  },
+  {
+    id: 1,
+    title: "b프로젝트",
+    image:
+      "https://blog.kakaocdn.net/dn/b8cCRr/btqBj57k7zX/70VkmwRvIB6b6Q9krOnIO0/img.jpg",
+    content: "그냥 아무거나 적은 것 제공",
+    image2: "https://gtechbooster.com/media/vue.jpg",
+  },
+  {
+    id: 1,
+    title: "b프로젝트",
+    image:
+      "https://blog.kakaocdn.net/dn/b8cCRr/btqBj57k7zX/70VkmwRvIB6b6Q9krOnIO0/img.jpg",
+    content: "그냥 아무거나 적은 것 제공",
+    image2: "https://gtechbooster.com/media/vue.jpg",
+  },
+  {
+    id: 1,
+    title: "b프로젝트",
+    image:
+      "https://blog.kakaocdn.net/dn/b8cCRr/btqBj57k7zX/70VkmwRvIB6b6Q9krOnIO0/img.jpg",
+    content: "그냥 아무거나 적은 것 제공",
+    image2: "https://gtechbooster.com/media/vue.jpg",
+  },
+  {
+    id: 1,
+    title: "b프로젝트",
+    image:
+      "https://blog.kakaocdn.net/dn/b8cCRr/btqBj57k7zX/70VkmwRvIB6b6Q9krOnIO0/img.jpg",
+    content: "그냥 아무거나 적은 것 제공",
+    image2: "https://gtechbooster.com/media/vue.jpg",
+  },
+  {
+    id: 1,
+    title: "b프로젝트",
+    image:
+      "https://blog.kakaocdn.net/dn/b8cCRr/btqBj57k7zX/70VkmwRvIB6b6Q9krOnIO0/img.jpg",
+    content: "그냥 아무거나 적은 것 제공",
+    image2: "https://gtechbooster.com/media/vue.jpg",
+  },
+  {
+    id: 1,
+    title: "b프로젝트",
+    image:
+      "https://blog.kakaocdn.net/dn/b8cCRr/btqBj57k7zX/70VkmwRvIB6b6Q9krOnIO0/img.jpg",
+    content: "그냥 아무거나 적은 것 제공",
+    image2: "https://gtechbooster.com/media/vue.jpg",
+  },
+  {
+    id: 1,
+    title: "b프로젝트",
+    image:
+      "https://blog.kakaocdn.net/dn/b8cCRr/btqBj57k7zX/70VkmwRvIB6b6Q9krOnIO0/img.jpg",
+    content: "그냥 아무거나 적은 것 제공",
+    image2: "https://gtechbooster.com/media/vue.jpg",
+  },
+  {
+    id: 1,
+    title: "b프로젝트",
+    image:
+      "https://blog.kakaocdn.net/dn/b8cCRr/btqBj57k7zX/70VkmwRvIB6b6Q9krOnIO0/img.jpg",
+    content: "그냥 아무거나 적은 것 제공",
+    image2: "https://gtechbooster.com/media/vue.jpg",
+  },
+  {
+    id: 1,
+    title: "b프로젝트",
+    image:
+      "https://blog.kakaocdn.net/dn/b8cCRr/btqBj57k7zX/70VkmwRvIB6b6Q9krOnIO0/img.jpg",
+    content: "그냥 아무거나 적은 것 제공",
+    image2: "https://gtechbooster.com/media/vue.jpg",
+  },
+  {
+    id: 1,
+    title: "b프로젝트",
+    image:
+      "https://blog.kakaocdn.net/dn/b8cCRr/btqBj57k7zX/70VkmwRvIB6b6Q9krOnIO0/img.jpg",
+    content: "그냥 아무거나 적은 것 제공",
+    image2: "https://gtechbooster.com/media/vue.jpg",
+  },
+  {
+    id: 1,
+    title: "b프로젝트",
+    image:
+      "https://blog.kakaocdn.net/dn/b8cCRr/btqBj57k7zX/70VkmwRvIB6b6Q9krOnIO0/img.jpg",
+    content: "그냥 아무거나 적은 것 제공",
+    image2: "https://gtechbooster.com/media/vue.jpg",
+  },
+  {
+    id: 1,
+    title: "b프로젝트",
+    image:
+      "https://blog.kakaocdn.net/dn/b8cCRr/btqBj57k7zX/70VkmwRvIB6b6Q9krOnIO0/img.jpg",
+    content: "그냥 아무거나 적은 것 제공",
+    image2: "https://gtechbooster.com/media/vue.jpg",
+  },
+];
