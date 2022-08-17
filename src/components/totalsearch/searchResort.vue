43c2d94026eefb07868c0db3394386b21966a069
ELuee <touch1027@naver.com>
2022-08-17 18:30:03 +0900

    modify the total search page

---
+++ src/components/totalsearch/searchResort.vue
@@ -0,0 +1,8 @@
+<template>
+  <p>this component is search resort</p>
+</template>
+<style scoped>
+p {
+  top: 100px;
+}
+</style>
--- src/views/TotalSearchView.vue
+++ src/views/TotalSearchView.vue
@@ -1,3 +1,16 @@
 <template>
   <p>total search page</p>
+  <searchResort></searchResort>
 </template>
+
+<script>
+import searchResort from "@/components/totalsearch/searchResort.vue";
+
+export default {
+  compononts: {
+    searchResort,
+  },
+};
+</script>
+
+<style scoped></style>
