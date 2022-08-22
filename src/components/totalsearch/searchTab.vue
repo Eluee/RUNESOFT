181ae3d9ae0bdccf7f780cfcf50a56fb965e966d
ELuee <touch1027@naver.com>
2022-08-22 18:26:49 +0900

    total search tab

--- src/components/totalsearch/searchTab.vue
+++ src/components/totalsearch/searchTab.vue
@@ -3,27 +3,28 @@
   <div class="search-body">
     <div class="search-ct">
       <div class="search-main">
-        <input type="text" class="search-main-input" />
+        <!--사실 이부분 뭔소리인지 모르겠지만 일단 진행을 위해 추가..-->
+        <input type="text" class="search-main-input" @keyup.enter="searching" />
       </div>
       <button class="search-button">검색</button>
       <div class="fillter-ct">
         <button
           class="fillter-icon"
-          :class="{ 'fillter-selected': fillter.notice }"
+          :class="{ 'fillter-selected': search.fillter.notice }"
           v-on:click="fillterselect('notice')"
         >
           <p>공지사항</p>
         </button>
         <button
           class="fillter-icon"
-          :class="{ 'fillter-selected': fillter.project }"
+          :class="{ 'fillter-selected': search.fillter.project }"
           v-on:click="fillterselect('project')"
         >
           <p>프로젝트</p>
         </button>
         <button
           class="fillter-icon"
-          :class="{ 'fillter-selected': fillter.all }"
+          :class="{ 'fillter-selected': search.fillter.all }"
           v-on:click="allselect()"
         >
           <svg
@@ -40,55 +41,92 @@
               stroke-linecap="round"
               stroke-linejoin="round"
               class="fillter-svg"
-              :class="{ 'fillter-svg-selected': fillter.all }"
+              :class="{ 'fillter-svg-selected': search.fillter.all }"
             />
           </svg>
           <p>전체</p>
         </button>
       </div>
+      <div class="key-word-ct">
+        <div
+          class="key-word-icon"
+          v-for="(list, i) in search.keyWord"
+          :key="i"
+          v-on:click="search.keyWord.delete(list)"
+        >
+          <p>{{ list }}</p>
+        </div>
+      </div>
+      <p class="resort-text">총 <span></span>건이 검색되었습니다</p>
     </div>
   </div>
 </template>
 <script>
+import { useStore } from "vuex";
+var keyWord = new Set();
+var store;
 export default {
+  setup() {},
   data() {
     return {
-      fillter: {
-        all: true,
-        project: true,
-        notice: true,
+      search: {
+        //입력한 값
+        inputText: "",
+        //키워드 값
+        keyWord,
+        //필터값
+        fillter: {
+          all: true,
+          project: true,
+          notice: true,
+        },
       },
     };
   },
   methods: {
     // all 필터 버튼
     allselect() {
-      console.log(this.fillter["all"]);
-      if (this.fillter["all"]) {
-        this.fillter["all"] = false;
-      } else {
-        this.fillter["all"] = true;
-      }
-      for (var key in this.fillter) {
-        this.fillter[key] = this.fillter["all"];
+      this.search.fillter["all"] = !this.search.fillter["all"];
+      for (var key in this.search.fillter) {
+        this.search.fillter[key] = this.search.fillter["all"];
       }
     },
     // 필터 선택 버튼
     fillterselect(key) {
-      if (this.fillter[key]) {
-        this.fillter[key] = false;
-        this.fillter["all"] = false;
+      if (this.search.fillter[key]) {
+        this.search.fillter[key] = false;
+        this.search.fillter["all"] = false;
       } else {
-        this.fillter[key] = true;
+        this.search.fillter[key] = true;
         this.examineallselect();
       }
     },
     examineallselect() {
       var fillterflag = true;
-      for (var key in this.fillter) {
-        fillterflag = this.fillter[key] && fillterflag;
+      for (var key in this.search.fillter) {
+        if (key != "all") {
+          fillterflag = this.search.fillter[key] && fillterflag;
+        }
       }
-      this.fillter["all"] = fillterflag;
+      this.search.fillter["all"] = fillterflag;
+    },
+    // 매개변수가 e로 설정되어있고 넘겨준것도 없지만 e에 접근이 가능하다...
+    searching(e) {
+      console.log("test32");
+      this.search.inputText = this.keywordSeparator(e.target.value);
+    },
+    keywordSeparator(text) {
+      if (text.length == 0) return;
+      //정규식
+      const reg = new RegExp(/#[ㄱ-힣|\w]+/, "g");
+      //정규식으로 문자열에서 키워드만 빼기
+      var keywordarr = text.match(reg);
+      if (keywordarr != null) {
+        keywordarr.forEach((item) => this.search.keyWord.add(item));
+        //키워드가 아닌 택스트 return
+        text = text.replace(reg, "");
+      }
+      return text;
     },
   },
 };
@@ -103,15 +141,15 @@ export default {
 .search-body {
   position: relative;
   width: 100%;
-  height: 250px;
+  height: auto;
 }
 .search-ct {
   position: relative;
   left: 50%;
-  top: 50px;
+  margin-top: 50px;
   transform: translateX(-50%);
   max-width: 600px;
-  height: 200px;
+  height: fit-content;
 }
 .search-main {
   position: relative;
@@ -181,5 +219,43 @@ export default {
 .fillter-svg-selected {
   fill: white;
   stroke: white;
+}
+.key-word-ct {
+  position: relative;
+  width: 100%;
+  height: auto;
+  margin-top: 80px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.key-word-icon {
+  width: fit-content;
+  height: 28px;
+
+  background: linear-gradient(
+    102.97deg,
+    rgba(108, 185, 255, 0.4) 0%,
+    rgba(173, 123, 255, 0.4) 100%
+  );
+  border-radius: 14px;
+  margin-right: 4px;
+  margin-bottom: 4px;
+}
+.key-word-icon > p {
+  position: relative;
+  font-size: 16px;
+  padding-left: 13px;
+  padding-right: 13px;
+  top: 50%;
+  transform: translateY(-50%);
+  color: white;
+}
+.resort-text {
+  position: relative;
+  text-align: left;
+  left: 0px;
+}
+.resort-text > span {
+  color: #e96060;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,46 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    return: {
+      allData: {
+        project: [
+          {
+            title: "mbti AI 분석 프로그램",
+            keyWord: ["AI", "mbti", "심리학", "python"],
+            link: "https://github.com/Eluee/VueExample",
+            infoLink: "https://www.naver.com",
+            explain:
+              "당신의 mbti를 AI로 분석해보자! 숨겨진 당신의 성격을 좀더 자세히 분석해드립니다.",
+          },
+          {
+            title: "드론 자율주행 프로젝트",
+            keyWord: ["AI", "드론", "자율주행", "python"],
+            link: "https://github.com/Eluee/VueExample",
+            infoLink: "https://www.naver.com",
+            explain:
+              "우리의 삶에 없어서는 안될 배달 드론으로 대체할 순 없을까? 드론자율주행을 파해쳐보았다.",
+          },
+          {
+            title: "AI가 매칭해주는 데이팅 어플",
+            keyWord: ["AI", "어플", "JAVA"],
+            link: "https://github.com/Eluee/VueExample",
+            infoLink: "https://www.naver.com",
+            explain:
+              "우리주변에는 누가 있을까? 주변친구와의 새로운 인연을 만들어주는 어플을 만들어보았다.",
+          },
+        ],
+        notice: [
+          {
+            title: "1차 신입 모집 공고",
+            keyWord: ["모집공고", "1차"],
+            infoLink: "https://www.naver.com",
+            explain: "RUNE SOFT에서 새로운 인원을 모집합니다.",
+          },
+        ],
+      },
+    },
+  },
   getters: {},
   mutations: {},
   actions: {},
